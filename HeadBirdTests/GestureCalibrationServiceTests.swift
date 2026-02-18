20d0c2b35574be655c85de7f20d3f2f373d71e25
Lelin07 <lelinpadhan27@gmail.com>
2026-02-18 19:35:01 +0530

    Expand calibration service test coverage and expectations

--- HeadBirdTests/GestureCalibrationServiceTests.swift
+++ HeadBirdTests/GestureCalibrationServiceTests.swift
@@ -92,6 +92,7 @@ final class GestureCalibrationServiceTests: XCTestCase {
             pitch: pitch,
             roll: 0,
             yaw: yaw,
+            sensorLocation: .default,
             rotationRate: CMRotationRate(x: 0, y: 0, z: 0),
             gravity: CMAcceleration(x: 0, y: -1, z: 0),
             userAcceleration: CMAcceleration(x: 0, y: 0, z: 0),
